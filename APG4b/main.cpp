0f030b0d0918fb0f9dbf955505cf22c7fd04a075
localmin <localmin9201009@gmail.com>
2019-03-19 22:45:59 +0900

    Answer bool pazzle

--- APG4b/main.cpp
+++ APG4b/main.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int a;
+int main() {
+  // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
+  bool a = 1; // true または false
+  bool b = 0;// true または false
+  bool c = 1;// true または false
 
-    cin >> a;
+  // ここから先は変更しないこと
 
-    cout << "Even" << endl;
+  if (a) {
+    cout << "At";
+  }
+  else {
+    cout << "Yo";
+  }
+
+  if (!a && b) {
+    cout << "Bo";
+  }
+  else if (!b || c) {
+    cout << "Co";
+  }
+
+  if (a && b && c) {
+    cout << "foo!";
+  }
+  else if (true && false) {
+    cout << "yeah!";
+  }
+  else if (!a || c) {
+    cout << "der";
+  }
+
+  cout << endl;
 }
